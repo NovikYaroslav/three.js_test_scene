8ae8bc279113a96c13e78adc8fdc452aad9e6cb3
Novik <yaroslavnowik@yandex.ru>
2024-04-20 16:11:09 +0200

    postprocessing effects added

--- src/App/App.js
+++ src/App/App.js
@@ -7,6 +7,12 @@ import { OrbitControls } from '@react-three/drei';
 import { Sculpture } from '../Sculpture/Sculpture';
 import Tree from '../Tree/Tree';
 import GrassLine from '../Grass-line/Grass-line';
+import {
+  Bloom,
+  DepthOfField,
+  EffectComposer,
+  Vignette,
+} from '@react-three/postprocessing';
 
 function App() {
   return (
@@ -21,12 +27,23 @@ function App() {
             <ambientLight intensity={0.3} />
             <directionalLight position={[10, 10, 10]} />
             <directionalLight position={[20, 10, 10]} />
+
             <Tree />
             <GrassLine />
             <Sculpture position={[0.25, -2.78, 0]} />
             <Plane />
           </Physics>
         </XR>
+        <EffectComposer>
+          <DepthOfField
+            focusDistance={0}
+            focalLength={0.02}
+            bokehScale={2}
+            height={480}
+          />
+          <Bloom luminanceThreshold={0} luminanceSmoothing={1} height={600} />
+          <Vignette eskil={false} offset={0.1} darkness={0.9} />
+        </EffectComposer>
       </Canvas>
     </div>
   );
